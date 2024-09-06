0e08cf776987b04d4cf13a590edf6af3433bda10
toleman84 <gdponcemarsiglia@gmail.com>
2024-09-06 16:05:58 -0300

    add ex.1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,23 +1,14 @@
 #/bin/bash
-decoded=$(echo "$1" | base64 -d)
+pass="$1"
+pass="${pass#'{xor}'}"
+decoded=$(echo "$pass" | base64 -d)
 
-key=$(echo -n "test" | xxd -p)
+decoded_pass_xor=""
+for ((i = 0; i < ${#decoded}; i++)); do
+    char="${decoded:$i:1}"
+    ascii_value=$(printf "%d" "'$char")
+    xor_result=$(( ascii_value ^ 95 ))
+    decoded_pass_xor+="$(printf "$(printf '\\x%x' $xor_result)")"
+done
 
-decoded_text=$(echo -n "$decoded" | xxd -p | awk -v key="$key" '
-  BEGIN {
-    split(key, k, "")
-    for (i = 1; i <= length(k); i += 2) {
-      k[i] = strtonum("0x" k[i] k[i + 1])
-    }
-  }
-  {
-    split($0, b, "")
-    for (i = 1; i <= length(b); i += 2) {
-      byte = strtonum("0x" b[i] b[i + 1])
-      xor = byte ^ k[(i - 1) % length(k) + 1]
-      printf("%c", xor)
-    }
-  }
-')
-
-echo "$decoded_text"
+echo -e "$decoded_pass_xor"
