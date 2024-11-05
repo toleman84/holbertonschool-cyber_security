e8ae8c73bfbc4cf1f7bc5260c943005abdb1e577
toleman84 <gdponcemarsiglia@gmail.com>
2024-11-05 13:21:31 -0300

    fix ex.6

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sS -scanflags URGACKPSHRSTSYNFIN -p $2 $1 -oN custom_scan.txt &> /dev/null
+sudo nmap -sS -scanflags URGACKPSHRSTSYNFIN -p $2 $1 -oN custom_scan.txt &>/dev/null
