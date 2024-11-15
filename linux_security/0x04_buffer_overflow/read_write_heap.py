bb2e07febc02bb1eeca6f91939f24abd1a49ded3
toleman84 <gdponcemarsiglia@gmail.com>
2024-11-15 00:20:25 -0300

    add files

---
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Usage: read_write_heap.py pid search_string replace_string
+where pid is the pid of the running process
+and strings are ASCII
+
+Output: you can print whatever you think is interesting
+
+On usage error, print an error message on stdout and exit with status code 1
+"""
+
