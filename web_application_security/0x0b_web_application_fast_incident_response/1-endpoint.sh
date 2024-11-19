48c644ef53058f4d19a2cce49aa0e241a902b38e
toleman84 <gdponcemarsiglia@gmail.com>
2024-11-19 10:59:31 -0300

    fix ex.1

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -2,4 +2,4 @@
 url="https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/text/2024/6/0090562cfd09781b394bfd9a051ca2f769e48c54.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20241119%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20241119T132137Z&X-Amz-Expires=345600&X-Amz-SignedHeaders=host&X-Amz-Signature=3449cd0a4b6a9833e03f33169b73f44e796dcd9a849020a600ab96541dbc0fc9"
 output_file="logs.txt"
 curl -o "$output_file" "$url"
-uniq -d "$output_file" | cut -c 53-54 | head -n 1
+uniq -d "$output_file" | cut -c 53-53 | head -n 1
