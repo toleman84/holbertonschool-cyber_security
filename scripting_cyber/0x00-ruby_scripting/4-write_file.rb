a0500f3281422e9c0f9feb60779cb96c2be7a8bd
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-13 16:17:29 -0300

    fix ex.4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -7,15 +7,5 @@ def merge_json_files(file1_path, file2_path)
     data1 = JSON.parse(file_content_1)
     data2 = JSON.parse(file_content_2)
 
-    merge_data = data1.merge(data2)
-
-    user_id_count = Hash.new(0)
-
-    merge_data.each do |item|
-        user_id_count[item["userId"]] += 1
-    end
-
-    user_id_count.each do |user_id, count|
-        puts "#{user_id}: #{count}"
-    end
+    merge_data = data1 + data2
 end
