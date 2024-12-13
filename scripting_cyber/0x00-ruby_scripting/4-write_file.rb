8b7d4df1182245343af40d5fe74bfc6253e59554
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-13 16:12:02 -0300

    add ex.4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+    file_content_1 = File.read(file1_path)
+    file_content_2 = File.read(file2_path)
+    data1 = JSON.parse(file_content_1)
+    data2 = JSON.parse(file_content_2)
+
+    merge_data = data1.merge(data2)
+
+    user_id_count = Hash.new(0)
+
+    merge_data.each do |item|
+        user_id_count[item["userId"]] += 1
+    end
+
+    user_id_count.each do |user_id, count|
+        puts "#{user_id}: #{count}"
+    end
+end
