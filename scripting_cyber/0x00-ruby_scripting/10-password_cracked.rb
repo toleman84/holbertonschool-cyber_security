a5ba92b112c18bb00cf314d2751e90108977d9c7
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 19:24:31 -0300

    add ex.10

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -1,1 +1,27 @@
 #!/usr/bin/env ruby
+
+require 'digest'
+
+def craker_pwd
+    if ARGV.empty?
+        puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
+    end
+    hashed_password = ARGV[0]
+    dictionary_file = ARGV[1]
+    found = false
+    # abrir archivo de passwords
+    File.foreach(dictionary_file) do |word|
+        word = word.chomp
+        # generar sha256 por cada word
+        hashed_word = Digest::SHA256.hexdigest(word)
+        # compararlo
+        if hashed_word == hashed_password
+            puts "Password found: #{word}"
+            found = true
+            break
+        end
+    end
+    puts "Password not found in dictionary." unless found
+end
+
+craker_pwd
