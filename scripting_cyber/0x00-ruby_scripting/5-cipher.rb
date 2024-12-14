eb793822e0ee978b9857a1d5d83675e0f877de9b
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 00:17:32 -0300

    add ex.5

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -1,1 +1,29 @@
 #!/usr/bin/env ruby
+
+class CaesarCipher
+
+    def initialize(shift)
+        @shift = shift % 26
+    end
+
+    def encrypt(message)
+        cipher(message, @shift)
+    end
+
+    def decrypt(message)
+        cipher(message, -@shift)
+    end
+
+    private
+
+    def cipher(message, shift)
+        message.chars.map do |char|
+            if char.match?(/[A-Za-z]/)
+                base = char.ord < 91 ? 'A'.ord : 'a'.ord
+                (((char.ord - base + shift) % 26) + base).chr
+            else
+                char
+            end
+        end.join
+    end
+end
