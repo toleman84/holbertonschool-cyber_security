042f003c3adafc76aa7787fb6205d98ce2338cd4
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 17:22:17 -0300

    add ex.9

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -1,1 +1,20 @@
 #!/usr/bin/env ruby
+
+require 'open-uri'
+
+def download
+    if ARGV.empty?
+        puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
+    else
+        url = ARGV[0]
+        path = ARGV[1]
+
+        URI.open(url) do |file|
+            File.open(path, 'wb') do |out|
+                out.write(file.read)
+            end
+        end
+    end
+end
+
+download
