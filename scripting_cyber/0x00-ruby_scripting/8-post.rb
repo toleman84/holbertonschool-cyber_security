b64508fd9175de1f75a1db1988a6a5d2d7a82bf1
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 16:12:55 -0300

    add ex.8

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+
+require 'net/http'
+
+def post_request(url, body_params)
+    uri = URI(url)
+    res = Net::HTTP.post_form(uri, body_params)
+
+    puts "Response status: #{res.code} #{res.message}"
+    puts "Response body:\n#{res.body}"
+end
