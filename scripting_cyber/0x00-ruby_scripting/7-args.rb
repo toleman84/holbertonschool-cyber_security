c44d383ef056f3b31bb6076629ff044669f9b0d0
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 15:59:39 -0300

    fix ex.7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
 def print_arguments
-    ARGV.each_with_index do |arg, index|
-        puts "#{index + 1}. #{arg}"
+    if ARGV.empty?
+        puts "No arguments provided."
+    else
+        puts "Arguments:"
+        ARGV.each_with_index do |arg, index|
+            puts "#{index + 1}. #{arg}"
+        end
     end
 end
