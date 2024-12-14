ad83d119ae2b9c86740990673d13b4f87c9a01f0
toleman84 <gdponcemarsiglia@gmail.com>
2024-12-14 15:39:53 -0300

    fix ex.6

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -6,11 +6,6 @@ def get_request(url)
         uri = URI(url)
         res = Net::HTTP.get_response(uri)
 
-        if res.body.nil? || res.body.empty?
-            puts "Response status: #{res.code} #{res.message}"
-            puts "Response body:\n{\n}"
-        else
-            puts "Response status: #{res.code} #{res.message}"
-            puts "Response body:\n#{res.body}"
-        end
+        puts "Response status: #{res.code} #{res.message}"
+        puts "Response body:\n#{res.body}"
 end
